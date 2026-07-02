358d63bc33519d28f8db03d005f509b66ae0d3b5
marceloquinenao-crypto <marcelo_quinenao@hotmail.com>
2026-07-02 19:20:52 -0400

    Agregando archivos de repaso

---
+++ ejercicios/core-6/script.js
@@ -0,0 +1,13 @@
+function sesion() {
+    let botonSesion = document.getElementById("cerrar")
+    botonSesion.innerText = "Cerrar Sesión";
+}
+
+function desaparecer() {
+    let botonDefinicion = document.getElementById("definicion")
+    botonDefinicion.remove()
+}
+
+function alerta() {
+    alert("Le diste like!!!");
+}
